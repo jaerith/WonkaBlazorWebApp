265e30066b074f76ed1e2ddfd9e33d88d8626672
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-05-19 10:17:43 -0400

    Initial revision.

---
+++ WonkaBlazorWebApp.Server/Controllers/ReportsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WonkaBlazorWebApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        /*
+        // GET: api/Reports
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        */
+
+        // GET: api/Reports/5
+        // [HttpGet("{id}", Name = "Get")]
+        public string GetReports(string RuleTreeId)
+        {
+            string sReportsJson = "";
+
+            string sBaseUrl = "http:/yourwebsite.here.net";
+
+            if (!String.IsNullOrEmpty(RuleTreeId))
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var ResponseMsg = client.GetAsync(sBaseUrl + "/api/Report?RuleTreeId=" + RuleTreeId).Result;
+
+                    sReportsJson = ResponseMsg.Content.ReadAsStringAsync().Result;
+
+                    // FOR TESTING ONLY
+                    if ((RuleTreeId == "TEST") && !sReportsJson.Contains("RuleSetFailMessages"))
+                    {
+                        int nLastCurlyBrace = sReportsJson.LastIndexOf("}");
+
+                        sReportsJson = sReportsJson.Remove(nLastCurlyBrace).Insert(nLastCurlyBrace, ", \"RuleSetFailMessages\": { } }]");
+                    }
+                }
+            }
+
+            return sReportsJson;
+        }
+
+        // POST: api/Reports
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Reports/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
