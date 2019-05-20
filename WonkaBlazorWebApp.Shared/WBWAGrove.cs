9505deaca296f83b60ef9cf0299a5ac0e776ea5e
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-05-20 18:08:15 -0400

    Initial revision of shared class.

---
+++ WonkaBlazorWebApp.Shared/WBWAGrove.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WonkaBlazorWebApp.Shared
+{
+    public class WBWAGrove
+    {
+        public string GroveId { get; set; }
+
+        public string GroveDesc { get; set; }
+
+        public List<string> GroveMembers { get; set; }
+
+        public bool SerializeToBlockchain { get; set; }
+
+    }
+}
