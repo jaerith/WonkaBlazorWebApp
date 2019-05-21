1a916f783a798f93d6a38f3eff6d5226d4ebb564
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-05-21 18:02:25 -0400

    Initial revision.

---
+++ WonkaBlazorWebApp.Shared/WBWAOwner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WonkaBlazorWebApp.Shared
+{
+    public class WBWAOwner
+    {
+        public string OwnerName { get; set; }
+
+        public string OwnerAddress { get; set; }
+
+        public string OwnerPassword { get; set; }
+
+    }
+}
+
