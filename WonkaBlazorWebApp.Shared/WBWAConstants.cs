577f9af075b4bf16478a49f8ba477a0b145a4c83
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-05-22 12:26:45 -0400

    Created static class to hold global constants.

---
+++ WonkaBlazorWebApp.Shared/WBWAConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WonkaBlazorWebApp.Shared
+{
+    public static class WBWAConstants
+    {
+        public const string CONST_REST_API_BASE_URL = "http:/yourwebsite.here.net";
+    }
+}
