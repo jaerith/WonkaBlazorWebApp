1a06fd861ac3b38d0dd11b7b30dd32ab3fe63c36
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-05-22 16:54:34 -0400

    Added more constants.

--- WonkaBlazorWebApp.Shared/WBWAConstants.cs
+++ WonkaBlazorWebApp.Shared/WBWAConstants.cs
@@ -6,6 +6,12 @@ namespace WonkaBlazorWebApp.Shared
 {
     public static class WBWAConstants
     {
-        public const string CONST_REST_API_BASE_URL = "http:/yourwebsite.here.net";
+        public const string CONST_REST_API_BASE_URL = "http://yourwebsite.here.net";
+
+        public const string CONST_VAT_CALC_RULETREE_URL =
+            "https://yourwebsite.here.net/files/VATCalculation.xml";
+
+        public const string CONST_VAT_CHK_RULETREE_2_URL =
+            "https://yourwebsite.here.net/files/VATPrecheckExample.xml";
     }
 }
