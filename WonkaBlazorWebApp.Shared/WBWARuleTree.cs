1653cddfa1f1ef083ad08c6434cdb4f12f40820f
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-05-22 16:52:05 -0400

    Initial revision.

---
+++ WonkaBlazorWebApp.Shared/WBWARuleTree.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WonkaBlazorWebApp.Shared
+{
+    public class WBWARuleTree
+    {
+        public string GroveId { get; set; }
+
+        public string RuleTreeId { get; set; }
+
+        public string RuleTreeOriginUrl { get; set; }
+
+        public bool UsingOrchestrationMode { get; set; }
+
+        public bool SerializeToBlockchain { get; set; }
+    }
+}
