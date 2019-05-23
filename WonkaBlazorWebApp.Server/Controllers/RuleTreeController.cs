58808e5cb90c7617f21a6aa7b1bad190d69c7246
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-05-23 11:38:02 -0400

    Created initial POST implementation of RuleTree controller.

--- WonkaBlazorWebApp.Server/Controllers/RuleTreeController.cs
+++ WonkaBlazorWebApp.Server/Controllers/RuleTreeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+
+using Newtonsoft.Json;
 
 namespace WonkaBlazorWebApp.Server.Controllers
 {
@@ -29,8 +32,30 @@ namespace WonkaBlazorWebApp.Server.Controllers
 
         // POST: api/RuleTree
         [HttpPost]
-        public void Post([FromBody] string value)
+        public string Post(WonkaBlazorWebApp.Shared.WBWARuleTree poRuleTree)
         {
+            string sResponseJsonMsg = "";
+
+            if (poRuleTree != null)
+            {
+                using (var client = new HttpClient())
+                {
+                    using (var request = new HttpRequestMessage(HttpMethod.Post, Shared.WBWAConstants.CONST_REST_API_BASE_URL + "/api/ruletree"))
+                    {
+                        var json = JsonConvert.SerializeObject(poRuleTree);
+                        using (var stringContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json"))
+                        {
+                            request.Content = stringContent;
+
+                            var responseMessage = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result;
+
+                            sResponseJsonMsg = responseMessage.Content.ReadAsStringAsync().Result;
+                        }
+                    }
+                }
+            }
+
+            return sResponseJsonMsg;
         }
 
         // PUT: api/RuleTree/5
