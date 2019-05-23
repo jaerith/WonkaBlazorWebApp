5c49dbb7a38026f3141922e81b0b8468b9abbd95
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-05-23 15:55:21 -0400

    Initial revision.

---
+++ WonkaBlazorWebApp.Shared/WBWAInvokeRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WonkaBlazorWebApp.Shared
+{
+    public class WBWAInvokeRecord
+    {
+        public Dictionary<string, string> RecordData;
+
+        public WBWAInvokeRecord()
+        {
+            InvokeOnChain = false;
+            RecordData    = new Dictionary<string, string>();
+        }
+
+        public void SetRuleTreeId(string psRuleTreeId)
+        {
+            RecordData["RuleTreeId"] = psRuleTreeId;
+        }
+
+        public bool InvokeOnChain { get; set; }
+    }
+}
