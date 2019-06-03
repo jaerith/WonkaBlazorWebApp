cf516d9179fcf1f178a17fb52dc66debbf8da897
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-06-03 17:39:49 -0400

    Initial revision.

---
+++ WonkaBlazorWebApp.Server/Controllers/ChainController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WonkaBlazorWebApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChainController : ControllerBase
+    {
+        // GET: api/Chain/5
+        // [HttpGet("{id}", Name = "Get")]
+        public string GetReports(string name = null)
+        {
+            string sAttrValue = "";
+
+            if (!String.IsNullOrEmpty(name))
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage ResponseMsg = new HttpResponseMessage();
+
+                    ResponseMsg = client.GetAsync(Shared.WBWAConstants.CONST_REST_API_BASE_URL + "/api/chain?type=attrval&id=" + name).Result;
+
+                    if (ResponseMsg.IsSuccessStatusCode)
+                        sAttrValue = ResponseMsg.Content.ReadAsStringAsync().Result;
+                    else
+                        sAttrValue = "Failed to retrieve value";
+                }
+            }
+
+            return sAttrValue;
+        }
+
+    }
+}
