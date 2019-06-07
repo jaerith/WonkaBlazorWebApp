78391178f1be0ba4adcfa09a414441946330aae9
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-06-07 18:00:22 -0400

    Added simple cache class.

---
+++ WonkaBlazorWebApp.Client/Shared/SimpleCache.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WonkaBlazorWebApp.Client.Shared
+{
+    public static class SimpleCache
+    {
+        public static WonkaBlazorWebApp.Shared.WBWARuleTree LastRuleTreeCreated { get; set; }
+    }
+}
